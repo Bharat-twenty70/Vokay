f2879d1c42cd0454a7ff8bc4e51608f0604a982c
Bharat <bharat.gude@twenty70.in>
2020-10-07 23:28:51 +0530

    background added

--- Vokay/ViewController.swift
+++ Vokay/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         print("project created")
+        self.view.backgroundColor = UIColor.blue
         
     }
     
